dc3b2abc3a80310417f3fba470d0c4bde18cbde3
JourBoon <unconfigured@null.spigotmc.org>
2023-09-28 15:33:49 +0200

    update

--- utils/maths_utils.py
+++ utils/maths_utils.py
@@ -8,3 +8,19 @@ class MathsUtils:
         Return the scalaire product of a and b.
         """
         return None
+    
+    @staticmethod
+    def tanh(x):
+        return np.tanh(x);
+
+    @staticmethod
+    def tanh_prime(x):
+        return 1-np.tanh(x)**2;
+
+    @staticmethod
+    def mse(y_true, y_pred):
+        return np.mean(np.power(y_true-y_pred, 2));
+
+    @staticmethod
+    def mse_prime(y_true, y_pred):
+        return 2*(y_pred-y_true)/y_true.size;
